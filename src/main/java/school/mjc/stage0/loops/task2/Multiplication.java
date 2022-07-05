bf2f22b7a8e28c286511225d4188cc63f91685fb
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-05 15:07:37 +0300

    Update Multiplication.java

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+                int startNum = 0;
+        while (startNum <= abs(multiplyByAndToInclusive)) {
+                System.out.println(startNum * multiplyByAndToInclusive);
+            startNum++;
+        }
     }
 }
