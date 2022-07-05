6688745a942c6bfabefb001d88622b2780efe76c
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-05 16:22:11 +0300

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+            int startnumWhile = 0;
+            StringBuilder result = new StringBuilder();
+            while (startnumWhile < chars.length) {
+                result.append(chars[startnumWhile]);
+            startnumWhile++;
+            }
+            System.out.println(result);
 
     }
 }
