2ea931f82dc964207ba0f8dba9d73fedc5d745e2
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-05 15:30:01 +0300

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+                int startNum = 1;
+        while (startNum <= printToInclusive) {
+            int k = 0;
+            int startNumWhile = 1;
+            while (startNumWhile <= startNum) {
+                if (startNum % startNumWhile == 0)
+                    k++;
+                startNumWhile++;
+            }
+            if (k <= 2)
+                System.out.println(startNum);
+            startNum++;
+        }
     }
 }
